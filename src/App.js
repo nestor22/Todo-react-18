431c6acf9499ba599769c5ab8b95c682208ce4a7
nestor22 <nestor.ge222@gmail.com>
2023-06-06 19:01:41 -0600

    first commit for TODO app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from "./platzi.webp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <lu>
+        <TodoItem />
+      </lu>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprendamos React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function TodoItem (){
+  return (
+    <li>
+      <span>V</span>
+      <p> lloar con la llorona </p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export default App;
